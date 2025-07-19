475d6704dc96341869041289b0fa2910f3ffb675
Sparsh <sparshmanhas111@gamil.com>
2025-07-19 21:14:40 +0530

    Changes in dm for instagram

--- api/webhooks/instagram.js
+++ api/webhooks/instagram.js
@@ -68,7 +68,7 @@ instagramRouter.post('/', async (req, res) => {
                 console.log(entry.id)
 
 // 758842513180249 slidesurf
-                const directMessage = await sendDM({
+                const directMessage = await sendPrivateMessage({
                     userId: entry.id ,
                     receiverId: entry.changes?.[0]?.value?.id || entry.messaging?.[0]?.sender?.id,
                     prompt: listener?.prompt,
